fca09b89892051159333e4bcb8847f72e457770e
soulwire <justin@soulwire.co.uk>
2015-05-14 13:51:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meteor Tween
+
+A Meteor package that wraps the [Moon tweening engine](https://github.com/moon/tween)
---
+++ package.js
@@ -0,0 +1,44 @@
+//------------------------------
+// Description
+//------------------------------
+
+Package.describe({
+  name: 'moonco:tween',
+  summary: 'Moon tweening engine',
+  version: '0.0.1',
+  git: 'https://github.com/moon/meteor-tween'
+});
+
+//------------------------------
+// Definition
+//------------------------------
+
+Package.onUse(function(api) {
+
+  api.versionsFrom('1.0.4.2');
+
+  //------------------------------
+  // Exports
+  //------------------------------
+
+  api.export('Tween');
+
+  //------------------------------
+  // Dependancies
+  //------------------------------
+
+  api.use([
+
+  ]);
+
+  //------------------------------
+  // Files
+  //------------------------------
+
+  api.addFiles([
+
+    'tween/tween.js'
+
+  ]);
+
+});
